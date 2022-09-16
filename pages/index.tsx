0fdcd1646af4e564c7329ddee1479fbdcfff8f1c
Szymon Miękus <miekusoft@gmail.com>
2022-09-16 18:23:21 +0200

    Row & Thumbnail components

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Header from "../components/Header";
 import Banner from "../components/Banner";
 import requests from "../utils/requests";
 import { Movie } from "../typings";
+import Row from "../components/Row";
 
 interface Props {
   originals: Movie[];
@@ -36,13 +37,14 @@ const Home: NextPage<Props> = ({
       <Header />
       <main className="relative pl-4 pb-25 lg:space-y-24 lg:pl-16">
         <Banner originals={originals} />
-        <section>
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
+        <section className="md:space-y-24">
+          <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
       {/* Modal */}
