9fba8d1bd4d5262863cfe9a70aa06b765b13e069
Szymon Miękus <miekusoft@gmail.com>
2022-09-18 23:57:34 +0200

    Display basic modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,22 +1,86 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MuiModal from "@mui/material/Modal";
 import { useRecoilState } from "recoil";
-import { modalState } from "../atoms/modalAtom";
+import { modalState, movieState } from "../atoms/modalAtom";
+import { XCircleIcon } from "@heroicons/react/24/outline";
+import { BASE_URL } from "../utils/requests";
+import { Element, Genre } from "../typings";
+import ReactPlayer from "react-player/lazy";
+import { FaPlay } from "react-icons/fa";
 
 function Modal() {
   const [showModal, setShowModal] = useRecoilState(modalState);
+  const [movie, setMovie] = useRecoilState(movieState);
+  const [trailer, setTrailer] = useState<string>("");
+  const [genres, setGenres] = useState<Genre[]>([]);
+  const [muted, setMuted] = useState<boolean>(true);
 
   const handleClose = (): void => {
     setShowModal(false);
   };
+
+  console.log(movie);
+
+  useEffect(() => {
+    if (!movie) return;
+
+    async function fetchMovie() {
+      const data = await fetch(
+        `${BASE_URL}/${movie?.media_type === "tv" ? "tv" : "movie"}/${
+          movie?.id
+        }?api_key=${
+          process.env.NEXT_PUBLIC_API_KEY
+        }&language=en-US&append_to_response=videos`
+      ).then((response) => response.json());
+
+      if (data?.videos) {
+        const index = data.videos.results.findIndex(
+          (element: Element) => element.type === "Trailer"
+        );
+        setTrailer(data.videos?.results[index]?.key);
+      }
+      if (data?.genres) {
+        setGenres(data.genres);
+      }
+    }
+
+    fetchMovie();
+  }, [movie]);
+
   return (
     <MuiModal
       open={showModal}
       onClose={handleClose}
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
+      className="fixed !top-7 left-0 z-50 mx-auto w-full max-w-5xl overflow-hidden overflow-y-scroll rounded-md scrollbar-hide"
     >
-      <></>
+      <>
+        <button
+          onClick={handleClose}
+          className="modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]"
+        >
+          <XCircleIcon className="h-6 w-6" />
+        </button>
+        <div className="relative pt-[56.25%]">
+          <ReactPlayer
+            url={`https://www.youtube.com/watch?v=${trailer}`}
+            width="100%"
+            height="100%"
+            style={{ position: "absolute", top: "0", left: "0" }}
+            playing
+            muted={muted}
+          />
+          <div>
+            <div>
+              <button className="flex items-center gap-x-2 rounded">
+                <FaPlay className="h-7 w-7 text-black" />
+                Play
+              </button>
+            </div>
+          </div>
+        </div>
+      </>
     </MuiModal>
   );
 }
